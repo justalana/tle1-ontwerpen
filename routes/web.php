7566f78df6b1eff8ba2cadfbc9f0dc223b00947f
Sander <sanderlandm@gmail.com>
2024-12-02 17:40:58 +0100

    ADD simple admin gate and a new default admin user

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,16 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
-Route::resource('branches', BranchController::class);
-Route::resource('companies', CompanyController::class);
 
-// TODO: Remove this route once it's no longer needed. This violates several OWASP things probably
-Route::get('/branch-dev', function () {
-    return view('branches.devpage');
-})
-    ->name('branchDevPage');
+Route::middleware('can:admin')->group(function () {
+
+    Route::get('/branch-dev', function () {
+        return view('branches.devpage');
+    })
+        ->name('branchDevPage');
+
+    Route::resource('branches', BranchController::class);
+    Route::resource('companies', CompanyController::class);
+
+});
+
