1d5faa6436d5f2dbbb3b4a7b7e4d8ca4bf1d0d5b
Alana le Feber <alana.l.f@upcmail.nl>
2024-12-03 14:38:02 +0100

    detail gerichte pagina css staat erin

--- resources/views/vacancies/details.blade.php
+++ resources/views/vacancies/details.blade.php
@@ -1,32 +1,16 @@
-@props(['title' => 'Open Hiring'])
-
-    <!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
-    @vite(['resources/css/style.css'])
+<x-site-layout>
     @vite(['resources/css/details.css'])
-</head>
-<body>
+    <header>
+        <div>
+            <h1 id="details-header">{{$vacancy->title}}</h1>
+        </div>
+    </header>
 
-<nav></nav>
-
-<header>
-    <div>
-        <h1>{{$vacancy->title}}</h1>
-    </div>
-</header>
-
-<main>
     <div id="detail-flexbox">
-        <div id="detail-flex">
+        <div>
             <img id="vacancy-image" src="{{asset('images/' . $vacancy->image_file_path)}}" alt="{{ $vacancy->image_file_path }}">
         </div>
-        <div id="detail-flex">
+        <div>
             <ul id="detail-list">
                 <li>Werkuren: {{$vacancy->work_hours}} uur per week</li>
                 <li>Contractduur: {{$vacancy->contract_duration}} dagen</li>
@@ -36,11 +20,14 @@
     </div>
 
     <div id="description">
-        <p>{{$vacancy->description}}</p>
+        <p id="description-text">{!!$vacancy->description!!}</p>
     </div>
-</main>
 
-<footer></footer>
+    <div>
+        <a href="">
+            <button class="button-light">Schrijf je in!</button>
+        </a>
+    </div>
 
-</body>
-</html>
+</x-site-layout>
+
