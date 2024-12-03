adc094ee5963649ee019f315224f439a900cc660
Alex <alexdusselaar@gmail.com>
2024-12-03 14:20:50 +0100

    story and preview sections added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,34 @@
             <p>Met Open Hiring heeft iedereen een eerlijke kans op een baan. Wie wil én kan werken, kan zó aan de slag. Zonder sollicitatiegesprek, zonder brief, zonder vragen. Met één druk op de knop. Open Hiring draait namelijk niet om diploma’s, maar om mensen. Niet om praatjes, maar om aanpakken.</p>
         </div>
         <img src="https://placehold.co/500x500" alt="decorative image">
-        </header>
+    </header>
+
+    <section id="stories">
+        <h2>Hoe jouw verhaal verschil maakt</h2>
+        <div>
+            <article class="werknemer">
+                <b>WERKNEMER</b>
+                <p>"Zonder sollicitatiegesprek is het makkelijker om aan het werk te gaan. Het is leuk, iedereen is aardig. Ik heb het hier naar mijn zin."</p>
+                <b>Adela</b>
+                <p>Vulploegmedewerker</p>
+            </article>
+            <article class="werkgever">
+                <b>WERKGEVER</b>
+                <p>“Je moet je vooroordelen en aannames kunnen loslaten, maar dan zul je vaak verrast worden door de kwaliteit en de persoon zelf.”</p>
+                <b>Gaby Westelaken</b>
+                <p>GWS dé schoonmaker</p>
+            </article>
+        </div>
+        <div class="buttons">
+            <a class="button-light">Vind ook een baan</a>
+            <a class="button-light">Een vacature plaatsen</a>
+        </div>
+
+    </section>
+    <section id="jobs-preview">
+        <h2>Openstaande vacatures voor iedereen</h2>
+        <p>Momenteel zijn er geen openstaande facatures beschikbaar.</p>
+        <a class="button-light">Bekijk alle vacatures</a>
+    </section>
+
 </x-site-layout>
