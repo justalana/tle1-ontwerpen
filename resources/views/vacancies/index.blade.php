946eb6031fcc96781bac15b6297c73344701db1e
Esse03 <essetijdelijk@gmail.com>
2024-12-03 12:53:47 +0100

    titel toevoeging vacancie database, css maar wel compleet kut en aanvoeging aan vacancies.index.blade

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -1,23 +1,30 @@
 <x-site-layout>
+    <header>
     <h1>Open Vacatures</h1>
-
+    </header>
+    <main>
     @if($vacancies->isNotEmpty())
-        <div>
             @foreach($vacancies as $vacancy)
-                <div>
-                <h1>!voeg titel toe plz?</h1>
-                <ul>
-                    <li>Salaris tussen: -{{$vacancy->salary_min}} en -{{$vacancy->salary_max}}</li>
-                    <li>{{$vacancy->work_hours}}</li>
-                    <li>{{$vacancy->contract_duration}}</li>
-                </ul>
+                <div class="column, item">
+                <div class="row">
+                    <img src="{{ asset('storage/' . $vacancy->image_file_path) }}" alt="afbeelding van {{$vacancy->name}}">
+                    <div class="column">
+                        <h2>{{$vacancy->title}}</h2>
+                        <ul>
+                            <li>Salaris tussen: €{{$vacancy->salary_min}},- en €{{$vacancy->salary_max}},-</li>
+                            <li>{{$vacancy->work_hours}} werkuren</li>
+                            <li>{{$vacancy->contract_duration}} maanden</li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
+                <div class="column">
                     <p>{{$vacancy->description}}</p>
+                    <a href="">Bekijk vacature</a>
+                </div>
                 </div>
             @endforeach
-        </div>
     @else
         <p>Helaas, geen open vacatures</p>
     @endif
+    </main>
 </x-site-layout>
