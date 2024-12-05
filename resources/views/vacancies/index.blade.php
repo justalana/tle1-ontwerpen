287f1243ffa9a9499149c0a7c9e3958ecb17c582
Sander <sanderlandm@gmail.com>
2024-12-05 12:20:36 +0100

    FIX small styling changes to vacancy index

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -1,46 +1,62 @@
+@vite(['resources/css/vacanciesIndex.css'])
+
 <x-site-layout>
-    @vite(['resources/css/vacanciesIndex.css'])
 
     <header>
-    <h1>Open Vacatures</h1>
+        <h1>Open Vacatures</h1>
     </header>
 
     @if($vacancies->isNotEmpty())
-            @foreach($vacancies as $vacancy)
 
-                <div class="article">
+        @foreach($vacancies as $vacancy)
+
+            <div class="article">
 
                 <div class="essentials">
 
-                    <img src="{{ asset('storage/' . $vacancy->image_file_path) }}" alt="afbeelding van {{$vacancy->name}}">
+                    <div class="image-container">
+                        <img src="{{ asset('storage/uploads/vacancyImages/' . $vacancy->image_file_path) }}"
+                             alt="{{ $vacancy->image_alt_text }}">
+                    </div>
 
                     <div class="column">
 
-                        <h2>{{$vacancy->title}}</h2>
+                        <h2>{{$vacancy->name}}</h2>
+
                         <ul>
-                            <li>
-                                @if($vacancy->salary_max)
-                                    Salaris tussen: €{{$vacancy->salary_min}},- en €{{$vacancy->salary_max}},-
-                                @else
-                                    Minimum salaris: €{{$vacancy->salary_min}}
-                                @endif
-                            </li>
-                            <li>{{$vacancy->work_hours}} werkuren</li>
+
+                            @if($vacancy->salary_max)
+                                <li>Salaris tussen: €{{ number_format((float)$vacancy->salary_min, 2) }},- en
+                                    €{{ number_format((float)$vacancy->salary_max, 2) }},- euro per uur
+                                </li>
+                            @else
+                                <li>Minimum salaris: €{{ number_format((float)$vacancy->salary_min, 2) }} euro per uur
+                                </li>
+                            @endif
+
+                            @if($vacancy->work_hours)
+                                <li>{{$vacancy->work_hours}} werkuren</li>
+                            @else
+                                <li>Werkuren niet bekend</li>
+                            @endif
+
                             <li>{{$vacancy->contract_duration}} dagen</li>
+
                         </ul>
 
                     </div>
 
                 </div>
 
-                    <div class="column">
-                        <p>{{$vacancy->description}}</p>
-                        <a class="button-pink" href="{{ route('vacancies.show', $vacancy -> id) }}">Bekijk vacature</a>
-                    </div>
-
+                <div class="column">
+                    <p>{{$vacancy->description}}</p>
+                    <a class="button-pink" href="{{ route('vacancies.show', $vacancy->id) }}">Bekijk vacature</a>
                 </div>
 
-            @endforeach
+            </div>
+
+        @endforeach
+
     @else
         <p>Helaas, geen open vacatures</p>
     @endif
