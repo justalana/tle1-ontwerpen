7702c2ed62efaf43603d661daeac7342dbe9d596
Sander <sanderlandm@gmail.com>
2024-12-05 15:58:35 +0100

    ADD changed direct links to redirects and added a link to the admin panel in site-layout.blade.php

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -11,31 +11,35 @@
     @vite(['resources/css/style.css'])
 </head>
 <body>
+
 <nav>
-    <a href="{{ '/'}}"><img src="{{asset('/images/logo.png')}}" alt="home"></a>
-    <a href="{{ route('vacancies.index') }}">Vacatures</a>
-    <a>Over Open Hiring</a>
-    <a>Contact</a>
+
+    <a href="{{ to_route('home') }}"><img src="{{ asset('/images/logo.png') }}" alt="home"></a>
+    <a href="{{ to_route('vacancies.index') }}">Vacatures</a>
+    <a href="">Over Open Hiring</a>
+    <a href="">Contact</a>
+
+    @can('admin')
+        <a href="{{ to_route('admin') }}">Admin</a>
+    @endcan
 
     @guest
         <div>
-            <a href="{{ route('login') }}">Log in</a>
+            <a href="{{ to_route('login') }}">Log in</a>
             <p>|</p>
-            <a href="{{ route('register') }}">Registreer</a>
+            <a href="{{ to_route('register') }}">Registreer</a>
         </div>
     @endguest
 
     @auth
-        @if(auth()->user())
             <div>
-                <a href="{{ route('profile.edit') }}">Profiel</a>
+                <a href="{{ to_route('profile.edit') }}">Profiel</a>
                 <p>|</p>
                 <form action="{{ route('logout') }}" method="post">
                     @csrf
                     <button type="submit">Log uit</button>
                 </form>
             </div>
-        @endif
     @endauth
 
 </nav>
