d48563c7a0d039c7f38675c1de44c94141ad9f8d
Sander <sanderlandm@gmail.com>
2024-12-09 15:27:23 +0100

    ADD Edit page for vacancies

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -52,7 +52,7 @@ class VacancyController extends Controller implements HasMiddleware
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'max:255'],
+            'name' => ['required', 'max:50'],
             'branch' => ['required', 'numeric'],
             'description' => ['required'],
             'salaryMin' => ['required', 'min:0', 'numeric'],
@@ -119,7 +119,9 @@ class VacancyController extends Controller implements HasMiddleware
      */
     public function edit(Vacancy $vacancy)
     {
-        //
+        $requirements = Requirement::all();
+
+        return view('vacancies.edit', ['requirements' => $requirements, 'vacancy' => $vacancy]);
     }
 
     /**
@@ -127,7 +129,53 @@ class VacancyController extends Controller implements HasMiddleware
      */
     public function update(Request $request, Vacancy $vacancy)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'max:50'],
+            'description' => ['required'],
+            'salaryMin' => ['required', 'min:0', 'numeric'],
+            'salaryMax' => ['min:0', 'numeric', 'nullable'],
+            'workHours' => ['min:0', 'numeric', 'nullable'],
+            'contractDuration' => ['required', 'min:1', 'numeric'],
+            'image' => ['nullable', 'image', 'mimes:jpeg,png,webp,gif,avif,apng', 'max:5000'],
+            'imageAltText' => ['required', 'max:255']
+        ]);
+
+        //Store the image in the correct folder and get a random filename if one was uploaded
+        if (isset($request->image)) {
+            $storagePath = public_path('storage/uploads/vacancyImages');
+
+            //Delete the old image
+            $oldImage = $storagePath . '/' . $vacancy->image_file_path;
+            unlink($oldImage);
+
+            //Save the new one
+            $newImage = $request->image;
+            $newName = Str::random(64) . '.' . $request->image->extension();
+            $newImage->move($storagePath, $newName);
+        }
+
+        //Clean the description before storing it to avoid any unsafe html
+        $htmlSanitizer = new HtmlSanitizer(
+            (new HtmlSanitizerConfig())->allowSafeElements()
+        );
+
+        $description = $htmlSanitizer->sanitize($request->description);
+
+        $vacancy->name = $request->name;
+        $vacancy->description = $description;
+        $vacancy->salary_min = $request->salaryMin;
+        $vacancy->salary_max = $request->salaryMax ?? null;
+        $vacancy->work_hours = $request->workHours ?? null;
+        $vacancy->contract_duration = $request->contractDuration;
+        $vacancy->image_file_path = $newName ?? $vacancy->image_file_path;
+        $vacancy->image_alt_text = $request->imageAltText;
+
+        $vacancy->update();
+
+        //Detach and/or attach the relevant requirements
+        $vacancy->requirements()->sync($request->requirements ?? []);
+
+        return to_route('vacancies.show', $vacancy);
     }
 
     /**
