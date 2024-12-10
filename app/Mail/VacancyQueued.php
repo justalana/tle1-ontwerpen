510dfeca9fde37397d9157ba8b0aaf574a7a12c9
Esse03 <essetijdelijk@gmail.com>
2024-12-10 13:00:39 +0100

    precies 0 keer gecommit, dus bij deze is er een dummy voor een email te sturen die momenteel wordt gestuurt bij het aanmaken van een vacature (na goedkeuring en test wordt alle dummy data verwijdert zodat ie gemerged kan worden)

--- app/Mail/VacancyQueued.php
+++ app/Mail/VacancyQueued.php
@@ -5,7 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -28,7 +27,6 @@ class VacancyQueued extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            from: new Address('dummy@dummy.com', 'Dummy'),
             subject: 'Vacancy Queued',
         );
     }
@@ -39,7 +37,7 @@ class VacancyQueued extends Mailable
     public function content(): Content
     {
         return new Content(
-            view('mail.layout.index')
+            markdown: 'mail.vacancy.queued',
         );
     }
 
