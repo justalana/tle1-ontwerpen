86a4847da773368f3b8debc597b953799aaa9164
Zoruasy <1076302@hr.nl>
2024-12-11 14:43:32 +0100

    goedkeuringen

--- resources/views/profile/employeedash.blade.php
+++ resources/views/profile/employeedash.blade.php
@@ -1,9 +1,7 @@
 @vite(['resources/css/employeedash.css'])
 <x-site-layout>
     <x-slot name="header">
-        <h2>
-            {{ __('Welkom op je Dashboard') }}
-        </h2>
+        <h2>{{ __('Welkom op je Dashboard') }}</h2>
     </x-slot>
 
     <div class="py-12">
@@ -11,10 +9,8 @@
             <div class="card">
                 <div class="text-center mb-8">
                     <h1 class="heading">Welkom, {{ auth()->user()->name }}!</h1>
-                    <p class="subheading">Je bent ingelogd op je dashboard.</p>
+                    <p class="subheading">Je bent ingelogd op je dashboard als werkgever.</p>
                 </div>
-
-
 
                 <!-- Werknemerspagina link -->
                 <div class="text-center mt-4">
@@ -22,6 +18,18 @@
                         Klik hier om naar je profiel te gaan
                     </a>
                 </div>
+
+
+                <!-- Meldingen en Goedkeuringen -->
+                <div class="text-center mb-8">
+                    <h2 class="heading">Vacature goedkeuringen(voorbeeld)</h2>
+
+{{--                    <p>Aantal goedkeuringen te verwerken: {{ $pendingApprovals }}</p>--}}
+{{--                    <a href="{{ route('approvals.list') }}" class="btn-profile-link">Bekijk goedkeuringen</a>--}}
+                </div>
+
+
+
             </div>
         </div>
     </div>
