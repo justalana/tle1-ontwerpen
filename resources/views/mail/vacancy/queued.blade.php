2fef491026bf11b807ec4acfe3943c75fd3b7201
Esse03 <essetijdelijk@gmail.com>
2024-12-19 12:00:15 +0100

    feedback bijwerking

--- resources/views/mail/vacancy/queued.blade.php
+++ resources/views/mail/vacancy/queued.blade.php
@@ -2,7 +2,6 @@
                 <table style="width: 100%; background-color: #ffffff; border-radius: 15px; padding: 20px;">
                     <tr>
                         <td>
-                            <!-- Email content -->
                             <h1 style="text-align: center;">U hebt succesvol gereageerd!</h1>
                             <h2>U heeft gereageerd op {{$vacancy->name}}.</h2>
                             <p style="font-size: 16px; color: #333333;">
